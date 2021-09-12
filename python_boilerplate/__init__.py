dce7eeedb379d8fd73c1095b75914c568fc4f731
Mathanraj-Sharma <rvmmathanraj@gmail.com>
2021-09-12 13:21:31 +0530

    add pre-commit config

--- python_boilerplate/__init__.py
+++ python_boilerplate/__init__.py
@@ -1,5 +1,5 @@
 """Top-level package for Python Boilerplate."""
 
 __author__ = """Mathanraj Sharma"""
-__email__ = 'rvmmathanraj@gmail.com'
-__version__ = '0.1.0'
+__email__ = "rvmmathanraj@gmail.com"
+__version__ = "0.1.0"
--- setup.py
+++ setup.py
@@ -4,41 +4,45 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = []
 
-test_requirements = ['pytest>=3', ]
+test_requirements = [
+    "pytest>=3",
+]
 
 setup(
     author="Mathanraj Sharma",
-    author_email='rvmmathanraj@gmail.com',
-    python_requires='>=3.6',
+    author_email="rvmmathanraj@gmail.com",
+    python_requires=">=3.6",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     description="Python Boilerplate contains all the boilerplate you need to create a Python package.",
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='python_boilerplate',
-    name='python_boilerplate',
-    packages=find_packages(include=['python_boilerplate', 'python_boilerplate.*']),
-    test_suite='tests',
+    keywords="python_boilerplate",
+    name="python_boilerplate",
+    packages=find_packages(
+        include=["python_boilerplate", "python_boilerplate.*"]
+    ),
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/Mathanraj-Sharma/python_boilerplate',
-    version='0.1.0',
+    url="https://github.com/Mathanraj-Sharma/python_boilerplate",
+    version="0.1.0",
     zip_safe=False,
 )
